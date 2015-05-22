184c7cb6b28cebfdf4afd718c1f2142f566e44cb
Rodrigo Ribeiro <rodrigo.sypriano.ribeiro@gmail.com>
2015-05-22 17:47:48 -0300

    Desenvovimento do sistema.

--- application/modules/default/controllers/VeiculoController.php
+++ application/modules/default/controllers/VeiculoController.php
@@ -11,53 +11,6 @@ class Default_VeiculoController extends Aplicacao_Controller_Action {
         $this->view->usuarios = $model->fetchAll($params);*/
     }
 
-    public function newAction() {
-        /*$form = new Aplicacao_Form_Usuario();
-        $this->view->form = $form;
-
-        if($this->_request->isPost()) {
-            if($form->isValid($this->data)) {
-                $model = new Application_Model_Usuario();
-                if($model->save($this->data))
-                    $this->_redirect ('/admin/usuario');
-            }
-        }*/
-    }
-
-    public function editAction() {
-        /*$model = new Application_Model_Usuario();
-        $form = new Aplicacao_Form_Usuario();
-        $id = (int) $this->_request->getParam("id",0);
-        $usuario = $model->find($id);
-        if($usuario) {
-            $form->populate ($usuario->toArray());
-            $form->getElement('id_perfil')->setValue($usuario->id_perfil);
-        }
-        $this->view->form = $form;
-
-        if ($this->_request->isPost()) {
-            if($id)
-                $this->data['id'] = $id;
-            if ($form->isValid($this->data)) {
-                if ($model->save($this->data))
-                    $this->_redirect ('/admin/usuario');
-            }
-        }*/
-    }
-
-    public function deleteAction() {
-        /*$model = new Application_Model_Usuario();
-        $form = new Aplicacao_Form_Usuario();
-        $id = (int) $this->_request->getParam("id",0);
-        if($id)
-            $this->data['id'] = $id;
-        if($model->delete($this->data))
-            $this->_redirect ('/admin/usuario');
-
-        $this->view->form = $form;
-        $this->view->error = "Erro ao excluir Usuario";*/
-    }
-
     /*
     public function preDispatch() {
         $auth = Zend_Auth::getInstance();
