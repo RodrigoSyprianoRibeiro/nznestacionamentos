b732cb61b93a5dbd088b3ae3a7a52466f3b0280b
Rodrigo Ribeiro <rodrigo.sypriano.ribeiro@gmail.com>
2015-05-25 10:45:50 -0300

    Desenvovimento do sistema.

--- application/modules/default/controllers/EstacionamentoController.php
+++ application/modules/default/controllers/EstacionamentoController.php
@@ -71,6 +71,19 @@ class Default_EstacionamentoController extends Aplicacao_Controller_Action {
         $this->view->error = "Erro ao excluir Usuario";*/
     }
 
+    public function configurarAction() {
+        /*$model = new Application_Model_Usuario();
+        $form = new Aplicacao_Form_Usuario();
+        $id = (int) $this->_request->getParam("id",0);
+        if($id)
+            $this->data['id'] = $id;
+        if($model->delete($this->data))
+            $this->_redirect ('/admin/usuario');
+
+        $this->view->form = $form;
+        $this->view->error = "Erro ao excluir Usuario";*/
+    }
+
     public function preDispatch() {
         $auth = Zend_Auth::getInstance();
         $auth->setStorage(new Zend_Auth_Storage_Session('admin'));
